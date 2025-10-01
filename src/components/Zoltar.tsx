1239b2ffa85142e40775f84fa88e736822b05eb9
Joseph <joseph@good-idea.studio>
2025-10-01 08:51:48 -0600

    feat: add meta, fortune teller CTA

--- src/components/Zoltar.tsx
+++ src/components/Zoltar.tsx
@@ -9,15 +9,21 @@ type ZoltarProps = {
 
 export const Zoltar: FC<ZoltarProps> = ({ handleTransition }) => {
   return (
-    <div className="pt-5">
-      <div className="w-[160px] md:w-[200px] mx-auto text-white">
+    <div className={cn('pt-5', 'flex flex-col items-center justify-center')}>
+      <div className={cn('w-[160px] md:w-[200px] mx-auto text-white')}>
         <AFILogo />
       </div>
       <button
-        className="cursor-pointer max-w-[450px] pt-4 px-5"
+        className="cursor-pointer max-w-[450px] px-5 pt-4 flex justify-center"
         onClick={handleTransition}
       >
         <Image src="/mystic.png" width={1080} height={1920} alt="" />
+      </button>
+      <button
+        className="cursor-pointer bg-purple text-white rounded-3xl px-4 text-sm"
+        onClick={handleTransition}
+      >
+        Reveal your vision
       </button>
     </div>
   )
