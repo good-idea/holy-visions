8cad29e44d5be3dad4eb58fa51dfa864815fc3d5
Joseph <joseph@good-idea.studio>
2025-10-03 11:25:58 -0600

    feat: set up sanity

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import {schemaTypes, structure} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +10,7 @@ export default defineConfig({
   projectId: 'bgbuxtjs',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({structure}), visionTool()],
 
   schema: {
     types: schemaTypes,
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,1 +1,31 @@
-export const schemaTypes = []
+import {defineType, defineField} from 'sanity'
+import {FaGear} from 'react-icons/fa6'
+import {StructureResolver} from 'sanity/structure'
+
+const settings = defineType({
+  name: 'settings',
+  title: 'Settings',
+  type: 'document',
+  icon: FaGear,
+  fields: [
+    defineField({
+      name: 'contestEnabled',
+      title: 'Contest Enabled',
+      description: 'Toggle this to OFF to remove the winning Vision from the site',
+      type: 'boolean',
+      validation: (Rule) => Rule.required(),
+      initialValue: true,
+    }),
+  ],
+  preview: {
+    prepare: () => {
+      return {title: 'Holy Visions'}
+    },
+  },
+})
+
+export const structure: StructureResolver = async (S, context) => {
+  return S.editor().id('settings').schemaType('settings').title('Settings')
+}
+
+export const schemaTypes = [settings]
